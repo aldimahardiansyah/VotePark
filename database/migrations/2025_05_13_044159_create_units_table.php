87815d3c57be50c127ed0114256c378a59e3d13c
aldimadiansyah <aldimdiansyah@gmail.com>
2025-05-13 17:15:38 +0700

    feat: make model & migration of units

---
+++ database/migrations/2025_05_13_044159_create_units_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('code')->unique();
+            $table->string('npp')->comment('Nilai Perbandingan Proporsional');
+            $table->string('wide')->comment('Luas');
+            $table->string('tower')->comment('A/B/C/D/E/F/G');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('units');
+    }
+};
